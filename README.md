b19bc34501f9dc55e76e6b59859ffd23bed0451d
Scott Corgan <scottcorgan@gmail.com>
2015-11-04 16:03:20 -0800

    Remove dependency on promise library

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # httpify
 
-Http in Node and Browserify, simply.
+Http in the browser and Node.js with no hassle.
 
 The beauty of this module is that it handles all browserify configurations for which module to load for server or browser.
  
@@ -15,21 +15,15 @@ npm install httpify --save
 When using with Browserify, this module returns a shimmed version of the [xhr](https://www.npmjs.org/package/xhr) module. When using with Node, it returns the raw [request](https://www.npmjs.org/package/request) module.
 
 ```js
-var httpify = require('httpify');
+var request = require('httpify')
 
-var req = httpify({
+var req = request({
   url: 'http://somewhere.com',
   method: 'GET',
   type: 'json'
 }, function (err, response, body) {
   // Do stuff
-});
-
-req.then(function (response) {
-  // Do stuff
-}, function (errResponse) {
-  // status: 400 - 599 
-});
+})
 ```
 
 See the documentation for [xhr](https://www.npmjs.org/package/xhr) and [request](https://www.npmjs.org/package/request) for a complete list of options.
--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-var Promise = require('promise');
-var request = require('request');
+var request = require('request')
+
+if (typeof Promise !== 'function') {
+  throw new TypeError('Please provide a Promise polyfill as your environment doesn\'t support them natively')
+}
 
 module.exports = function (options, callback) {
+
   return new Promise(function (resolve, reject) {
+
     request(options, function (err, response, body) {
-      var status = (response) ? response.statusCode : 0;
-      callback = callback || function () {};
-      
+
+      var status = (response) ? response.statusCode : 0
+      callback = callback || function () {}
+
       if (err) {
-        callback(err);
-        reject(err);
-        return 
-      }
-      
-      try{
-        response.body = JSON.parse(body);
-      }
-      catch (e) {}
-      
-      if (status >= 400 && status < 600) {
-        callback(null, response, response.body);
-        reject(response);
+        callback(err)
+        reject(err)
         return
       }
-      
-      callback(null, response, response.body);
-      resolve(response);
-    });
-  });
-};
+
+      try{
+        response.body = JSON.parse(body)
+      }
+      catch (e) {}
+
+      if (status >= 400 && status < 600) {
+        callback(null, response, response.body)
+        reject(response)
+        return
+      }
+
+      callback(null, response, response.body)
+      resolve(response)
+    })
+  })
+}
